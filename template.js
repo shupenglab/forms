acc259c76d2328b235f65744b2c65608f3657d6b
yanyaoer <yanyaoer@gmail.com>
2013-06-05 14:46:40 +0800

    modify tpl,css with purecss.io construct

--- template.js
+++ template.js
@@ -1,11 +1,11 @@
-exports.textbox = '<div class="attribute"><label>{{title}}<input type="text"></input></label></div>';
+exports.textbox = '<div class="pure-control-group"><label>{{title}}</label><input type="text"></input></div>';
 
-exports.checkbox = '<div class="attribute"><label><input type="checkbox"></input>{{title}}</label></div>';
+exports.checkbox = '<div class="pure-control-group"><label><input type="checkbox"></input>{{title}}</label></div>';
 
-exports.select = '<div class="attribute"><label>{{title}}<select></select></label></div>';
+exports.select = '<div class="pure-control-group"><label>{{title}}</label><select></select></div>';
 
-exports.object = '<div class="attribute"><label>{{title}}</label><div class="nested"></div></div>';
+exports.object = '<div class="pure-control-group"><label>{{title}}</label><div class="nested"></div></div>';
 
-exports.repeats = '<div class="attribute"><label>{{title}}</label><a class="add">add</a><div class="repeats"></div></div>';
+exports.repeats = '<div class="pure-control-group"><label>{{title}}</label><a class="add">add</a><div class="repeats"></div></div>';
 
 exports.controls = '<div class="controls"><a class="remove">remove</a> <a class="add">add</a></div>';
